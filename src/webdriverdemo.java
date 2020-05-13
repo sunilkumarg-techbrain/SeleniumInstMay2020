57716d26278ce85e55470358b637aa4c7e6f5723
Sunil kumar <Sunil kumar@192.168.1.6>
2020-05-13 11:16:03 +0530

    Initial Commit. Added 'webdriverdemo.java' file

---
+++ src/webdriverdemo.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class webdriverdemo {
+   public static void main(String[] args) {
+     System.setProperty("webdriver.chrome.driver","C:\\Selenium_Sunil\\drivers\\chromedriver.exe");  
+     // Instantiate a ChromeDriver class.       
+     WebDriver driver=new ChromeDriver();  
+     String baseUrl = "http://demo.guru99.com/test/newtours/";
+      String expectedTitle = "Welcome: Mercury Tours";
+      String actualTitle = "";
+      // launch Fire fox and direct it to the Base URL
+      driver.get(baseUrl);
+      // get the actual value of the title
+      actualTitle = driver.getTitle();
+      /*
+       * compare the actual title of the page with the expected one and print
+       * the result as "Passed" or "Failed"
+       */
+      if (actualTitle.contentEquals(expectedTitle)){
+          System.out.println("Test Passed!");
+      } else {
+          System.out.println("Test Failed");
+      }
+     
+      //close Fire fox
+      driver.close();
+    
+    
+   }
+}
